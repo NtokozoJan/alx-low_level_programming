9a56f4e6a5ba14b3999e4af936f7d5974fa84d13
NtokozoJan <ntokozomahwayi067@gmail.com>
2023-08-13 19:49:59 +0200

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,13 @@ int main(void)
 
 	for (w = 0 ; w < 10 ; w++)
 	{
-		if (w == 9)
-			putchar(w + '0');
-		else
+		putchar(w + '0');
+		if (w != 9)
 		{
-			putchar(w + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
