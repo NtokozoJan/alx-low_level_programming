7dbce08f2cfbc9f45d059488544ae5249c9c9de6
NtokozoJan <ntokozomahwayi067@gmail.com>
2023-08-13 20:21:34 +0200

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *Return; always 0
+ */
+int main(void)
+{
+	int t = '0';
+	int g = '0';
+	int d = '0';
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		for (g = '0'; g <= '9'; g++)
+		{
+			for (t = '0'; t <= '9'; t++)
+			{
+				if (!((t == g) || (g == d) || (g > t) || (d > g)))
+				{
+					putchar(d);
+					putchar(g);
+					putchar(t);
+					if (!(t == '9' && d == '7' && g == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
