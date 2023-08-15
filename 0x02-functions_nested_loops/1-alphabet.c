b25221df460479906d046e0e04b7ea6b4261e285
NtokozoJan <ntokozomahwayi067@gmail.com>
2023-08-15 19:40:40 +0200

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - In lowercase
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	char ap;
+
+	for (ap = 'a'; ap <= 'z'; ap++)
+		_putchar(ap);
+	_putchar('\n');
+}
