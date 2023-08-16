90713b2b6f22dda583e80ab1bcd3d3d2f081e387
NtokozoJan <ntokozomahwayi067@gmail.com>
2023-08-16 21:51:28 +0200

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,37 @@
 
 int main(void)
 {
-	int a = 0;
-	unsigned long int b = 0, c = 1, after = 0;
+	unsigned long int a;
+	unsigned long int prev = 1;
+	unsigned long int futu = 2;
+	unsigned long int b = 1000000000;
+	unsigned long int prev1;
+	unsigned long int prev2;
+	unsigned long int futu1;
+	unsigned long int futu2;
 
-	while (a < 98)
+	printf("%lu", prev);
+
+	for (a = 1; a < 91; a++)
 	{
-		after = b + c;
-		b = c;
-		c = after;
-		printf("%lu", after);
-
-		if (a < 97)
-			printf(", ");
-		++a;
+		printf(", %lu", futu);
+		futu += prev;
+		prev = futu - prev;
 	}
-	putchar('\n');
+	prev1 = (prev / b);
+	prev2 = (prev % b);
+	futu1 = (futu / b);
+	futu2 = (futu % b);
+
+	for (a = 92; a < 99; ++a)
+	{
+		printf(",%lu", futu1 + (futu2 / b));
+		printf("%lu", futu2 % b);
+		futu1 = futu1 + prev1;
+		prev1 = futu1 - prev1;
+		futu2 = futu2 + prev2;
+		prev2 = futu2 - prev2;
+	}
+	printf("\n");
 	return (0);
 }
