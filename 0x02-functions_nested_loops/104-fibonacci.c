d5e36691a783780e9fbb8c7cb375deb7ce4d4c1c
NtokozoJan <ntokozomahwayi067@gmail.com>
2023-08-16 21:34:22 +0200

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -  program that finds and prints the first 98 Fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	unsigned long int b = 0, c = 1, after = 0;
+
+	while (a < 98)
+	{
+		after = b + c;
+		b = c;
+		c = after;
+		printf("%lu", after);
+
+		if (a < 97)
+			printf(", ");
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
