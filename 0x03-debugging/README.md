13259ad4f1b85e15017006fb1210bcfca7efad0a
NtokozoJan <ntokozomahwayi067@gmail.com>
2023-08-17 17:56:25 +0200

    0-main.c

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+My first debugging read me file
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - positiv or negative
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0 )
+	{
+		printf("%d is %s\n", i, "negative");
+	}
+	else if (i > 0)
+	{
+		printf("%d is %s\n", i, "positive");
+	}
+	else
+	{
+		printf("%d is %s\n", i, "zero");
+	}
+	return;
+}
