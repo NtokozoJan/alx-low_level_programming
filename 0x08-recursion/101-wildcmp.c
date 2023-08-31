0b451dcfb55bdaf725b69c81ac8dd5329631ee6b
NtokozoJan <ntokozomahwayi067@gmail.com>
2023-08-31 22:07:58 +0200

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -22,13 +22,13 @@ int wildcmp(char *s1, char *s2)
 		if (*(s2 + 1) == '*')
 			return (wildcmp(s1, s2 + 1));
 
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-				return (1);
-			s1++;
-		}
 		if (*s1 == '\0' && *(s2 + 1) == '\0')
+			return (1);
+
+		if (wildcmp(s1, s2 + 1))
+			return (1);
+
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
 			return (1);
 	}
 	return (0);
