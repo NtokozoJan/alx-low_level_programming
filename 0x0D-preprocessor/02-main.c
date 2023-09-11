3b78281d8d4219b7055edf37d38796963ddb4f1f
NtokozoJan <ntokozomahwayi067@gmail.com>
2023-09-11 15:13:50 +0200

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define UNUSED(x) (void)(x)
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	UNUSED(printf("%s\n", __FILE__));
+	return (0);
+}
