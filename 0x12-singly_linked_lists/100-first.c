3c6f790166655d7c7ac83ff24b9ee2cc579282d0
NtokozoJan <ntokozomahwayi067@gmail.com>
2023-09-21 17:54:45 +0200

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "100-first.h"
+#include <stdio.h>
+
+/**
+ * print_before_main - Function that prints a specific message before main.
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+/**
+ * own - Ntokozo Mahwayi
+ */
---
+++ 0x12-singly_linked_lists/100-first.h
@@ -0,0 +1,9 @@
+#ifndef __FIRST_H__
+#define __FIRST_H__
+
+/**
+ * print_before_main - Function that prints a specific message before main.
+ */
+void print_before_main(void) __attribute__((constructor));
+
+#endif /* __FIRST_H__ */
